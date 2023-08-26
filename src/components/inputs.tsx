1dfb47c02e0dfa09a2f65ba3cfe671f15a59afd7
Eddie Sholl <eddie.sholl@elementree.io>
2023-08-26 14:41:34 +1000

    feat: basic input form layout

--- src/components/inputs.tsx
+++ src/components/inputs.tsx
@@ -1,10 +1,49 @@
-import { Button } from 'flowbite-react';
-
 export const Inputs = () => {
   return (
     <div style={{ width: '50%' }}>
       <h3>Inputs</h3>
-      <Button>This is a test button</Button>
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h4 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">
+          Business activities
+        </h4>
+        <form action="#">
+          <div className="grid gap-4 grid-cols-2">
+            <span className="col-span-2 mb-4 text-xl font-bold text-gray-900 dark:text-white">
+              When did these activities happen?
+            </span>
+            <div className="w-full">
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Start date
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type start date"
+              />
+            </div>
+            <div className="w-full">
+              <label
+                htmlFor="name"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+              >
+                End date
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type end date"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
