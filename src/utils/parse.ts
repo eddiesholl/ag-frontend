ca4674c2e0837b2509c761880ed0340b34e6c288
Eddie Sholl <eddie.sholl@elementree.io>
2023-08-29 11:08:49 +1000

    chore: removing dead code

--- src/utils/parse.ts
+++
@@ -1,9 +0,0 @@
-export const stringToInt = (value: string) => {
-  const parsed = parseInt(value, 10);
-  return isNaN(parsed) ? undefined : parsed;
-};
-
-export const stringToFloat = (value: string) => {
-  const parsed = parseFloat(value);
-  return isNaN(parsed) ? undefined : parsed;
-};
